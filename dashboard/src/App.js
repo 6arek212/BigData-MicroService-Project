691c0f552027a7072946e3556559d9b677274eef
Wissam111 <riki_kabha@hotmail.com>
2023-03-05 20:18:53 +0200

    fixing css

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -5,19 +5,18 @@ import NavBar from "./presentation/components/NavBar/NavBar";
 import Orders from "./presentation/pages/Orders/Orders";
 import Dashboard from "./presentation/pages/Dashboard/Dashboard";
 import Analyze from "./presentation/pages/Analyze/Analyze";
-
 function App() {
   return (
-    <BrowserRouter>
-      <div className="app-wrapper">
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/orders" element={<Orders />} />
-          <Route path="/analyze" element={<Analyze />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+      <BrowserRouter>
+        <div className="app-wrapper">
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/orders" element={<Orders />} />
+            <Route path="/analyze" element={<Analyze />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
   );
 }
 
--- dashboard/src/presentation/pages/Orders/Orders.js
+++ dashboard/src/presentation/pages/Orders/Orders.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Orders.css";
 import OrderCard from "../../components/OrderCard/OrderCard";
 import Select from "react-select";
@@ -8,20 +8,45 @@ import { FiPhoneCall } from "react-icons/fi";
 import OrderStatusCard from "../../components/OrderStatusCard/OrderStatusCard";
 import OrderItemInfo from "../../components/OrderItemInfo/OrderItemInfo";
 import ItemToppings from "../../components/ItemToppings/ItemToppings";
-
 import TextField from "@mui/material/TextField";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
 
+import moment from "moment";
+
+const BASE_URL = "http://localhost:4000/api";
+
 function Orders() {
   const [date, setDate] = useState(new Date());
   const [showToppings, setShowToppings] = useState(false);
+  const [region, setRegion] = useState("");
+  const [orders, setOrders] = useState([]);
   const options = [
+    { value: "South", label: "South" },
     { value: "Haifa", label: "Haifa" },
-    { value: "TelAviv", label: "Tel Aviv" },
-    { value: "Zikhron", label: "Zikhron Ya'akov" },
+    { value: "Center", label: "Center" },
+    { value: "North", label: "North" },
+    { value: "Dan", label: "Dan" },
   ];
+
+  const format = "yyyy-MM-DDTHH:mm";
+
+  const fetchData = async () => {
+    const startDate = moment(date).startOf("day").format(format);
+    const endDate = moment(date).endOf("day").format(format);
+    console.log(startDate);
+    console.log(endDate);
+    const res = await fetch(
+      BASE_URL +
+        `/search?startDate=${startDate}&endDate=${endDate}&storeName=${region}`
+    );
+    console.log(res);
+  };
+  useEffect(() => {
+    fetchData();
+  }, [date]);
+  console.log(region);
 
   return (
     <div className="page-container">
@@ -31,7 +56,11 @@ function Orders() {
             <div className="select-option">
               <span>Branch</span>
               <div className="select-branch-options">
-                <Select options={options} />
+                <Select
+                  options={options}
+                  defaultValue="South"
+                  onChange={(option) => setRegion(option.value)}
+                />
               </div>
             </div>
             <div className="select-option">
@@ -41,7 +70,7 @@ function Orders() {
                   <DesktopDatePicker
                     inputFormat="MM/DD/YYYY"
                     value={date}
-                    onChange={(value) => setDate(value)}
+                    onChange={(value) => setDate(value["$d"])}
                     renderInput={(params) => (
                       <TextField
                         {...params}
