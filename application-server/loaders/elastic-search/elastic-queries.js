49bcfb34b2b1a97b01586e2d0130787999090e87
Wissam111 <riki_kabha@hotmail.com>
2023-03-05 23:41:39 +0200

    Elasticsearch get all results + fixing bugs

--- application-server/loaders/elastic-search/elastic-queries.js
+++ application-server/loaders/elastic-search/elastic-queries.js
@@ -1,63 +1,59 @@
-const moment = require('moment');
-
-
+const moment = require("moment");
 
 const makeElasticSearchQueries = (client) => {
-
-
-    const searchOrdersByDate = async ({ startDate, endDate, storeName, page, pageSize }) => {
-        const filter = []
-        const must = []
-        const q = {
-            index: 'orders',
-            query: {
-                bool: {
-                    must: must,
-                    filter: filter
-                },
-            },
-            size: 30,
-            from: 0,
-        }
-
-
-        if (storeName) {
-            must.push({
-                match: {
-                    store_name: storeName
-                }
-            })
-        }
-
-
-
-        if (startDate && endDate) {
-            filter.push({
-                range: {
-                    createdAt: { gte: startDate, lte: endDate }
-                }
-            })
-        }
-
-        if (page && pageSize) {
-            q.size = pageSize
-            q.from = (page - 1) * pageSize
-        }
-
-
-        const data = await client.search(q)
-
-        return data.hits.hits
+  const searchOrdersByDate = async ({
+    startDate,
+    endDate,
+    storeName,
+    page,
+    pageSize,
+  }) => {
+    let allRecords = [];
+    const filter = [];
+    const must = [];
+    const q = {
+      index: "orders",
+      scroll: "10s",
+      query: {
+        bool: {
+          must: must,
+          filter: filter,
+        },
+      },
+    };
+    if (storeName) {
+      must.push({
+        match: {
+          store_name: storeName,
+        },
+      });
+    }
+    if (startDate && endDate) {
+      filter.push({
+        range: {
+          createdAt: { gte: startDate, lte: endDate },
+        },
+      });
     }
 
-
-    return {
-        searchOrdersByDate
+    var { _scroll_id, hits } = await client.search(q);
+    while (hits && hits.hits.length) {
+      // Append all new hits
+      allRecords.push(...hits.hits);
+      var { _scroll_id, hits } = await client.scroll({
+        scroll_id: _scroll_id,
+        scroll: "10s",
+      });
     }
-}
 
+    return allRecords;
+  };
 
+  return {
+    searchOrdersByDate,
+  };
+};
 
 module.exports = {
-    makeElasticSearchQueries
-}
+  makeElasticSearchQueries,
+};
